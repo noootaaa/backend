3f20618fd6a8b129b17827eeedd51520849454df
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-05-26 19:41:26 +0200

    feat: update fields and add relationships

--- apps/products/models/product.ts
+++ apps/products/models/product.ts
@@ -1,10 +1,43 @@
-import { BaseModel, beforeCreate, column } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeCreate, belongsTo, column } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 import { randomUUID } from 'node:crypto'
+import ProductCategory from '#apps/products/models/product_category'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Organization from '#apps/organization/models/organization'
 
 export default class Product extends BaseModel {
   @column({ isPrimary: true })
   declare id: string
+
+  @column()
+  declare name: string
+
+  @column()
+  declare description?: string
+
+  @column()
+  declare price: number
+
+  @column()
+  declare amount: number
+
+  @column()
+  declare thhumbnail?: string
+
+  @column()
+  declare categoryId: string
+
+  @column()
+  declare organizationId: string
+
+  @column()
+  declare customFields: { [key: string]: string }
+
+  @belongsTo(() => ProductCategory)
+  declare category: BelongsTo<typeof ProductCategory>
+
+  @belongsTo(() => Organization)
+  declare organization: BelongsTo<typeof Organization>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
@@ -14,8 +47,6 @@ export default class Product extends BaseModel {
 
   @beforeCreate()
   public static async generateUuid(model: Product) {
-    if (model.$dirty.id) {
-      model.id = randomUUID()
-    }
+    model.id = randomUUID()
   }
 }
