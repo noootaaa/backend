9e81b941cabaab89217a3cf48f22be66546dbed9
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-05-27 12:00:07 +0200

    feat: wip products controller

--- apps/products/controllers/products_controller.ts
+++ apps/products/controllers/products_controller.ts
@@ -1,4 +1,13 @@
 import { inject } from '@adonisjs/core'
+import ProductService from '#apps/products/services/product_service'
 
 @inject()
-export default class ProductsController {}
+export default class ProductsController {
+  constructor(
+    protected productService: ProductService,
+  ) {}
+  
+  async index() {
+    return "Route /v1/products work!"
+  }
+}
--- apps/products/routes.ts
+++ apps/products/routes.ts
@@ -1,5 +1,13 @@
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+
+const ProductsController = () => import('#apps/products/controllers/products_controller')
 
 router.group(() => {
+  router.group(() => {
+    router.get('/', [ProductsController, 'index'])
 
-}).prefix('products')
+  })
+  .prefix('/v1/products')
+  .use(middleware.auth({ guards: ['jwt'] }))
+})
